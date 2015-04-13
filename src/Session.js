f646aa07b8f4bac2ce7f74e2c926494dd41d28a4
ironhee <iron@ediket.com>
2015-04-13 18:08:42 +0900

    refactoring session

--- src/Session.js
+++ src/Session.js
@@ -22,7 +22,14 @@ var ResourceSession = function ResourceSession (resources, url, options) {
     throw new TypeError();
   }
 
-  options = options || {};
+  options = _.defaults(options || {}, {
+    syncronizer: {
+      post: function () {},
+      patch: function () {},
+      delete: function () {},
+      get: function () {}
+    },
+  });
 
   this.url = url;
   this.cameras = {};
@@ -44,69 +51,167 @@ var ResourceSession = function ResourceSession (resources, url, options) {
 
   }, this);
 
-  this.listenTo(this.resources, 'add', function (resource) {
-
-    if (_.contains(this.removed, resource)) {
-      _.pull(this.removed, resources);
-      return;
-    }
-    this.added.push(resource);
-
-  });
-
-  this.listenTo(this.resources, 'remove', function (resource) {
-
-    if (_.contains(this.added, resource)) {
-      _.pull(this.added, resources);
-      return;
-    }
-    this.removed.push(resource);
-
-  });
-
-  this.listenTo(this.resources, 'change', function (resource) {
-
-    this.changed.push(resource);
-    this._getCamera(resource).capture();
-
-  });
-
   this.resources.each(function (resource) {
 
     _.each(resource.getLinks(), function (resource, referKey) {
 
-      this.linkSessions.push(
-        new ResourceSession(resource,
-          url + 'links/' + referKey + '/', options)
-      );
+      this._createLinkSession(resource, referKey, options);
 
     }, this);
 
   }, this);
 
+  this.listenTo(this.resources, 'add', this._trackAdded);
+  this.listenTo(this.resources, 'remove', this._trackRemoved);
+  this.listenTo(this.resources, 'change', this._trackChanged);
 
-  this.listenTo(this.resources, 'add:link', function (resource, referKey) {
-
-    this.linkSessions.push(
-      new ResourceSession(resource,
-        url + 'links/' + referKey + '/', options)
-    );
-
-  });
-
-  this.listenTo(this.resources, 'remove:link', function (resource, referKey) {
-
-    var session = _.find(this.linkSessions, function (session) {
-      return session.resources === resource;
-    });
-    _.pull(this.linkSessions, session);
-
-  });
+  this.listenTo(this.resources, 'add:link', this._trackLinkAdded);
+  this.listenTo(this.resources, 'remove:link', this._trackLinkRemoved);
 
 };
 
 
 _.extend(ResourceSession.prototype, Events, {
+
+  commit: function () {
+
+    this._syncResources();
+    this._resetCameras();
+    this._resetTrackers();
+
+    this._commitLinkSessions();
+
+  },
+
+  _commitLinkSessions: function () {
+
+    _.each(this.linkSessions, function (session) {
+      return session.commit();
+    });
+
+  },
+
+  _syncResources: function () {
+
+    this._patchChangedResources();
+    this._postAddedResources();
+    this._deleteRemovedResources();
+
+  },
+
+  _patchChangedResources: function () {
+
+    var patchData = _.chain(this.changed)
+      .filter(function (resource) {
+        var camera = this._getCamera(resource);
+        return !_.isEqual(camera.first(), camera.last());
+      }.bind(this))
+      .map(function (resource) {
+        return serializer.serialize(resource, { include: [] }).data;
+      })
+      .value();
+
+    if (!_.isEmpty(patchData)) {
+      this.syncronizer.patch(this.url, {
+        data: patchData
+      });
+    }
+
+  },
+
+  _postAddedResources: function () {
+
+    var postData = _.map(this.added, function (resource) {
+      return serializer.serialize(resource, { include: [] }).data;
+    });
+
+    if (!_.isEmpty(postData)) {
+      this.syncronizer.post(this.url, {
+        data: postData
+      });
+    }
+
+  },
+
+  _deleteRemovedResources: function () {
+
+    var deleteData = _.map(this.removed, function (resource) {
+      return resource.toLinkage();
+    });
+
+    if (!_.isEmpty(deleteData)) {
+      this.syncronizer.delete(this.url, {
+        data: deleteData
+      });
+    }
+
+  },
+
+  _resetCameras: function () {
+
+    _.each(this.changed, function (resource) {
+      var camera = this._getCamera(resource);
+      camera.snapshots = camera.snapshots.slice(-1);
+    }, this);
+
+    _.each(this.added, function (resource) {
+      this._createCamera(resource).capture();
+    }, this);
+
+    _.each(this.removed, function (resource) {
+      this._removeCamera(resource);
+    }, this);
+
+  },
+
+  _resetTrackers: function () {
+
+    this.addad = [];
+    this.removed = [];
+    this.changed = [];
+
+  },
+
+  _trackAdded: function (resource) {
+
+    if (_.contains(this.removed, resource)) {
+      _.pull(this.removed, this.resources);
+      return;
+    }
+    this.added.push(resource);
+
+  },
+
+  _trackRemoved: function (resource) {
+
+    if (_.contains(this.added, resource)) {
+      _.pull(this.added, this.resources);
+      return;
+    }
+    this.removed.push(resource);
+
+  },
+
+  _trackChanged: function (resource) {
+
+    this.changed.push(resource);
+    this._getCamera(resource).capture();
+
+  },
+
+  _trackLinkAdded: function (resource, referKey) {
+
+    this._createLinkSession(resource, referKey, {
+      syncronizer: this.syncronizer
+    });
+
+  },
+
+  _trackLinkRemoved: function (resource) {
+
+    this._removeLinkSession(resource);
+
+  },
 
   _createCamera: function (resource) {
 
@@ -133,71 +238,23 @@ _.extend(ResourceSession.prototype, Events, {
 
   },
 
-  commit: function () {
+  _createLinkSession: function (resource, key, options) {
 
-    // CHANGED
-    var patchData = _.chain(this.changed)
-      .filter(function (resource) {
-        var camera = this._getCamera(resource);
-        return !_.isEqual(camera.first(), camera.last());
-      }.bind(this))
-      .map(function (resource) {
-        return serializer.serialize(resource, { include: [] }).data;
-      })
-      .value();
+    this.linkSessions.push(
+      new ResourceSession(resource,
+        this.url + 'links/' + key + '/', options)
+    );
 
-    if (!_.isEmpty(patchData)) {
-      this.syncronizer.patch(this.url, {
-        data: patchData
-      });
-    }
+  },
 
-    // ADDED
-    var postData = _.map(this.added, function (resource) {
-      return serializer.serialize(resource, { include: [] }).data;
+  _removeLinkSession: function (resource) {
+
+    var session = _.find(this.linkSessions, function (session) {
+      return session.resources === resource;
     });
+    _.pull(this.linkSessions, session);
 
-    if (!_.isEmpty(postData)) {
-      this.syncronizer.post(this.url, {
-        data: postData
-      });
-    }
-
-    // REMOVED
-    var deleteData = _.map(this.removed, function (resource) {
-      return resource.toLinkage();
-    });
-
-    if (!_.isEmpty(deleteData)) {
-      this.syncronizer.delete(this.url, {
-        data: deleteData
-      });
-    }
-
-    // Reset Camera
-    _.each(this.changed, function (resource) {
-      var camera = this._getCamera(resource);
-      camera.snapshots = camera.snapshots.slice(-1);
-    }, this);
-
-    _.each(this.added, function (resource) {
-      this._createCamera(resource).capture();
-    }, this);
-
-    _.each(this.removed, function (resource) {
-      this._removeCamera(resource);
-    }, this);
-
-    // Reset change log
-    this.addad = [];
-    this.removed = [];
-    this.changed = [];
-
-    _.each(this.linkSessions, function (session) {
-      return session.commit();
-    });
-
-  }
+  },
 
 });
 
